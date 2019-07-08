7cdd2982a1705e05c76c06e93c66a7b9167c6327
蒋鹏辉 <aaron@lwork.com>
2019-07-08 15:06:39 +0800

    增加setting模块

---
+++ setting/src/main/java/com/example/aaron/compsapp/setting/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.aaron.compsapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aaron.compsapp.setting.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
