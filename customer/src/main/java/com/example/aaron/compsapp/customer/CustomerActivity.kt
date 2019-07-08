8269ae8cac100e91ff67a1829635a737a37c093d
蒋鹏辉 <aaron@lwork.com>
2019-07-08 14:07:07 +0800

    增加customer模块

---
+++ customer/src/main/java/com/example/aaron/compsapp/customer/CustomerActivity.kt
@@ -0,0 +1,23 @@
+package com.example.aaron.compsapp.customer
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+
+        val btn = findViewById<Button>(R.id.customer_btn_to_details)
+        btn.setOnClickListener { v -> toDetails(v) }
+    }
+
+    private fun toDetails(v: View) {
+        val intent = Intent(this, DetailsActivity::class.java)
+        startActivity(intent)
+    }
+}
